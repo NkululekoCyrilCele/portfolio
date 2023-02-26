91e679bece77d6eaf326194c0f7d077dd404d868
Nkululeko <nkululeko.cele@gmail.com>
2023-02-26 13:34:40 +0200

    Update files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portfoliodb',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.TextField(max_length=20)
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
