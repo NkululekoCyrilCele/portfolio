bda3f06af90daad0d630ce45f6c2938b9ffbe8dc
Nkululeko <nkululeko.cele@gmail.com>
2023-02-28 10:37:50 +0200

    Update models and HTML

--- projects/models.py
+++ projects/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.TextField(max_length=20)
+    title = models.CharField(max_length=20)
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    summary = models.TextField(max_length=200)
+    url = models.URLField(default=None)
+
+    class Meta:
+        ordering = ('title', )
+
+    def __str__(self):
+        return self.title
